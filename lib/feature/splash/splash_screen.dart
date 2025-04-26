0ea61266435a47b891b11bd027cc8be4bf84d6c7
Mohamed Hisham <mohamedhisham67751@gmail.com>
2025-04-26 14:57:35 +0300

    feat: Integrate flutter_native_splash for improved splash screen experience
    This commit integrates the `flutter_native_splash` package to provide a customizable native splash screen for Android, iOS, and web platforms.
    
    The following changes were made:
    
    - Added `flutter_native_splash` as a dependency in `pubspec.yaml` and configured it with a background color.
    - Created `flutter_native_splash.yaml` to define splash screen settings, including background color and platform-specific configurations.
    - Updated Android resources to use a background image for the splash screen and configure window styles.
    - Updated iOS launch screen storyboard to use a background image.
    - Modified iOS configuration files (Podfile, xcconfig, plist) to integrate with CocoaPods and configure the splash screen.
    - Added web configurations to customize the splash screen.
    - Added a conditional navigator push to the home route in the splash screen to avoid errors.
    
    These changes provide a more polished and customizable splash screen experience across all supported platforms.

--- lib/feature/splash/splash_screen.dart
+++ lib/feature/splash/splash_screen.dart
@@ -36,6 +36,7 @@ class _SplashScreenState extends State<SplashScreen>
 
     _controller.forward();
     Future.delayed(Duration(seconds: 3), () {
+      // ignore: use_build_context_synchronously
       Navigator.pushReplacementNamed(context, AppRoutes.homeRoute);
     });
   }
