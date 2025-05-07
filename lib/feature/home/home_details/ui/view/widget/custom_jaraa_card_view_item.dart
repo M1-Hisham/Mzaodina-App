283331c8b3e7837bbfda2429d8f6f16b867cb903
Ahmed Elgammal <elgammal2024@gmail.com>
2025-05-07 16:09:59 +0300

    feat: Refactor CustomJaraaCardViewItem and CustomJaraaPriceCard for improved readability and structure

--- lib/feature/home/home_details/ui/view/widget/custom_jaraa_card_view_item.dart
+++ lib/feature/home/home_details/ui/view/widget/custom_jaraa_card_view_item.dart
@@ -19,123 +19,125 @@ class _CustomQadimCardViewItemState extends State<CustomJaraaCardViewItem> {
   DateTime eventTimeFromApi = DateTime.parse('2025-05-04 18:00:00');
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.all(8.r),
-          decoration: BoxDecoration(
-            color: R.colors.whiteLight,
-            borderRadius: BorderRadius.circular(16.r),
-            border: Border.all(color: R.colors.whiteColor2),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8.r),
+            decoration: BoxDecoration(
+              color: R.colors.whiteLight,
+              borderRadius: BorderRadius.circular(16.r),
+              border: Border.all(color: R.colors.whiteColor2),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
 
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    R.images.phoneImagePng,
-                    width: 120.w,
-                    height: 158.h,
-                  ),
-                  SizedBox(width: 16.w),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 8.h),
-                          child: Text(
-                            'مزاد على ايفون 16 برو من ابل',
-                            style: R.textStyles.font16BlackW500Light,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      R.images.phoneImagePng,
+                      width: 120.w,
+                      height: 158.h,
+                    ),
+                    SizedBox(width: 16.w),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 8.h),
+                            child: Text(
+                              'مزاد على ايفون 16 برو من ابل',
+                              style: R.textStyles.font16BlackW500Light,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 12.h),
-                        CustomBlocBuilderCountdown(
-                          eventTime: eventTimeFromApi,
-                          progressColor: R.colors.greenColor,
-                          backgroundColor: R.colors.greenColor2,
-                        ),
-                        SizedBox(height: 12.h),
-                        CoustomRowItem(
-                          title: 'السعر بالأسواق',
-                          price: '1,000.00  ',
-                        ),
-                        CoustomRowItem(title: 'اعلى مزاد', price: '600.00  '),
+                          SizedBox(height: 12.h),
+                          CustomBlocBuilderCountdown(
+                            eventTime: eventTimeFromApi,
+                            progressColor: R.colors.greenColor,
+                            backgroundColor: R.colors.greenColor2,
+                          ),
+                          SizedBox(height: 12.h),
+                          CoustomRowItem(
+                            title: 'السعر بالأسواق',
+                            price: '1,000.00  ',
+                          ),
+                          CoustomRowItem(title: 'اعلى مزاد', price: '600.00  '),
 
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 6.h),
-                          color: R.colors.transparent,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                'المزاود',
-                                style: R.textStyles.font12Grey3W500Light,
-                              ),
-                              Spacer(),
-                              Text(
-                                'لايوجد',
-                                style: R.textStyles.font16primaryW600Light,
-                              ),
-                            ],
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 6.h),
+                            color: R.colors.transparent,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'المزاود',
+                                  style: R.textStyles.font12Grey3W500Light,
+                                ),
+                                Spacer(),
+                                Text(
+                                  'لايوجد',
+                                  style: R.textStyles.font16primaryW600Light,
+                                ),
+                              ],
+                            ),
                           ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16.h),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: CustomElevatedButton(
+                        text: 'عرض التفاصيل',
+                        onPressed: () {
+                          Navigator.pushNamed(
+                            context,
+                            AppRoutes.homeDetailsJaraaScreenRoute,
+                            arguments: {'eventTime': eventTimeFromApi},
+                          );
+                        },
+                        backgroundColor: R.colors.primaryColorLight,
+                        borderRadius: 8.r,
+                        heigth: 40.h,
+                        textStyle: R.textStyles.font12GreyW500Light.copyWith(
+                          color: R.colors.whiteLight,
                         ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16.h),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: CustomElevatedButton(
-                      text: 'عرض التفاصيل',
-                      onPressed: () {
-                        Navigator.pushNamed(
-                          context,
-                          AppRoutes.homeDetailsJaraaScreenRoute,
-                          arguments: {'eventTime': eventTimeFromApi},
-                        );
-                      },
-                      backgroundColor: R.colors.primaryColorLight,
-                      borderRadius: 8.r,
-                      heigth: 40.h,
-                      textStyle: R.textStyles.font12GreyW500Light.copyWith(
-                        color: R.colors.whiteLight,
                       ),
                     ),
-                  ),
-                  SizedBox(width: 11.w),
-                  Expanded(
-                    child: CustomElevatedButton(
-                      textDirection: TextDirection.rtl,
-                      text: 'مشاركة',
-                      onPressed: () {},
-                      backgroundColor: R.colors.colorUnSelected,
-                      borderRadius: 8.r,
-                      heigth: 40.h,
-                      textStyle: R.textStyles.font14BlackW500Light.copyWith(
-                        color: R.colors.primaryColorLight,
-                      ),
-                      icon: SvgPicture.asset(
-                        R.images.shareIcon,
-                        width: 20.w,
-                        height: 20.h,
+                    SizedBox(width: 11.w),
+                    Expanded(
+                      child: CustomElevatedButton(
+                        textDirection: TextDirection.rtl,
+                        text: 'مشاركة',
+                        onPressed: () {},
+                        backgroundColor: R.colors.colorUnSelected,
+                        borderRadius: 8.r,
+                        heigth: 40.h,
+                        textStyle: R.textStyles.font14BlackW500Light.copyWith(
+                          color: R.colors.primaryColorLight,
+                        ),
+                        icon: SvgPicture.asset(
+                          R.images.shareIcon,
+                          width: 20.w,
+                          height: 20.h,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
