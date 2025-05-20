68677ebd8c284bca5e25d6c03eceddeb0472b88f
Mohamed Hisham <mohamedhisham67751@gmail.com>
2025-05-20 17:47:49 +0300

    feat: Display username in home app bar using UserDataCubit
    - Integrated UserDataCubit to fetch and display the username in the home app bar.
    - Updated CustomAppBarHome to use BlocBuilder to react to UserDataCubit state changes.
    - Added UserDataLoading, UserDataNotLoggedIn, UserDataSuccess, and UserDataError states to handle different scenarios.
    - Modified HomeScreen to provide UserDataCubit using BlocProvider and trigger initial data fetch.

--- lib/feature/home/ui/view/home_screen.dart
+++ lib/feature/home/ui/view/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mzaodina_app/core/DI/setup_get_it.dart';
 import 'package:mzaodina_app/core/resources/resources.dart';
 import 'package:mzaodina_app/feature/home/ui/view/widget/custom_app_bar_home.dart';
 import 'package:mzaodina_app/feature/home/ui/view/widget/custom_tap_view.dart';
+import 'package:mzaodina_app/feature/profile/view_model/user_data_cubit/user_data_cubit.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,7 +15,10 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: R.colors.whiteLight,
       body: Column(
         children: [
-          CustomAppBarHome(),
+          BlocProvider(
+            create: (context) => getIt<UserDataCubit>()..fetchUserData(),
+            child: CustomAppBarHome(),
+          ),
           // SizedBox(height: 20),
           Expanded(child: CustomTapView()),
         ],
